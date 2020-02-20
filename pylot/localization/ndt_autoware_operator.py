360d567b9420047c811f4cdf03cd2e9f95b34e3d
Ionel Gog <gogionel@gmail.com>
2020-02-19 16:24:27 -0800

    Include speed in localization message.

--- pylot/localization/ndt_autoware_operator.py
+++ pylot/localization/ndt_autoware_operator.py
@@ -1,6 +1,7 @@
 import erdos
 import rospy
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Float32
 from tf.transformations import euler_from_quaternion
 
 from pylot.utils import CanBus, Location, Rotation, Transform
@@ -21,28 +22,33 @@ class NDTAutowareOperator(erdos.Operator):
         self._logger = erdos.utils.setup_logging(name, log_file_name)
         self._csv_logger = erdos.utils.setup_csv_logging(
             name + '-csv', csv_file_name)
+        self._forward_speed = 0
 
     @staticmethod
     def connect():
         return [erdos.WriteStream()]
 
     def on_pose_update(self, data):
-        loc = Location(data.pose.point.x, data.pose.point.y, data.pose.point.z)
+        loc = Location(data.pose.position.x, data.pose.position.y,
+                       data.pose.position.z)
         quaternion = [
             data.pose.orientation.x, data.pose.orientation.y,
             data.pose.orientation.z, data.pose.orientation.w
         ]
         roll, pitch, yaw = euler_from_quaternion(quaternion)
         rotation = Rotation(pitch, yaw, roll)
-        # TODO: Set forward_speed.
-        forward_speed = 0
         timestamp = erdos.Timestamp(coordinates=[data.header.seq])
-        can_bus = CanBus(Transform(loc, rotation), forward_speed)
-        print('Location {}; Rotation {}'.format(loc, rotation))
+        can_bus = CanBus(Transform(loc, rotation), self._forward_speed)
+        print('Localization {}'.format(can_bus))
         self._can_bus_stream.send(erdos.Message(timestamp, can_bus))
         self._can_bus_stream.send(erdos.WatermarkMessage(timestamp))
+
+    def on_velocity_update(self, data):
+        self._forward_speed = data
 
     def run(self):
         rospy.init_node(self._name, anonymous=True, disable_signals=True)
         rospy.Subscriber(self._topic_name, PoseStamped, self.on_pose_update)
+        rospy.Subscriber('/estimated_vel_mps', Float32,
+                         self.on_velocity_update)
         rospy.spin()
