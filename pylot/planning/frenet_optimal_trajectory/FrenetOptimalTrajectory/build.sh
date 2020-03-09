101e0040cf07df2531659460430c6cc8cef1f551
Edward Fang <edward.fang@berkeley.edu>
2020-03-09 11:18:08 -0700

    Removing src code

--- pylot/planning/frenet_optimal_trajectory/FrenetOptimalTrajectory/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y libeigen3-dev clang cmake
-if [ ! -d "build" ]; then
-  mkdir build
-fi
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --target all -- -j 8
