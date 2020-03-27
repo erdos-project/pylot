35bfe725120ef46173130f5f3c4fcb9a0f1904c1
amandhar <amandhar@berkeley.edu>
2020-03-27 08:50:17 -0700

    Added label support to the Deepsort object tracker (#84)

--- pylot/perception/tracking/deep_sort_tracker.py
+++ pylot/perception/tracking/deep_sort_tracker.py
@@ -16,7 +16,6 @@ class MultiObjectDeepSORTTracker(MultiObjectTracker):
         # Initialize the deepsort object, which has a tracker object within it
         self._deepsort = deepsort_rbc(
             wt_path=flags.deep_sort_tracker_person_weights_path)
-        self.tracker = None
 
     def reinitialize(self, frame, obstacles):
         """ Reinitializes a multiple obstacle tracker.
@@ -29,36 +28,37 @@ class MultiObjectDeepSORTTracker(MultiObjectTracker):
         # update tracker with new detections
         self.track(frame, obstacles)
 
-    def track(self, frame, obstacles=None):
+    def track(self, frame, obstacles=[]):
         """ Tracks obstacles in a frame.
 
         Args:
             frame (:py:class:`~pylot.perception.camera_frame.CameraFrame`):
                 Frame to track in.
         """
+        bboxes, labels, confidence_scores = [], [], []
+        for obstacle in obstacles:
+            bboxes.append(obstacle.bounding_box.as_width_height_bbox())
+            labels.append(obstacle.label)
+            confidence_scores.append(obstacle.confidence)
+        # TODO: Remove this if-check and figure out why it breaks the deepsort
+        # matching. If removed, calling tracker.predict() without an update
+        # messes up tracker ages and matches are no longer made, just new tracks
+        # each time step
         if obstacles:
-            bboxes = [
-                obstacle.bounding_box.as_width_height_bbox()
-                for obstacle in obstacles
-            ]
-            confidence_scores = [obstacle.confidence for obstacle in obstacles]
-            self.tracker, detections_class = self._deepsort.run_deep_sort(
-                frame.frame, confidence_scores, bboxes)
-        if self.tracker:
-            obstacles = []
-            for track in self.tracker.tracks:
-                if not track.is_confirmed() or track.time_since_update > 1:
-                    continue
-                # Converts x, y, w, h bbox to tlbr bbox (top left and bottom
-                # right coords).
-                bbox = track.to_tlbr()
-                # TODO: DeepSort doesn't maintain labels of tracked obstacled.
-                # Hence, we tag tracked obstacles as actors.
-                # Converts to xmin, xmax, ymin, ymax format.
-                obstacles.append(
-                    DetectedObstacle(
-                        BoundingBox2D(int(bbox[0]), int(bbox[2]), int(bbox[1]),
-                                      int(bbox[3])), 0, 'actor',
-                        track.track_id))
-            return True, obstacles
-        return False, []
+            # If obstacles is empty, this should move existing tracks one step
+            # ahead. Otherwise, it will update trackers with detections.
+            self._deepsort.run_deep_sort(
+                frame.frame, confidence_scores, bboxes, labels)
+        tracked_obstacles = []
+        for track in self._deepsort.tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            # Converts x, y, w, h bbox to tlbr bbox (top left and bottom
+            # right coords).
+            bbox = track.to_tlbr()
+            # Converts to xmin, xmax, ymin, ymax format.
+            bbox_2d = BoundingBox2D(
+                int(bbox[0]), int(bbox[2]), int(bbox[1]), int(bbox[3]))
+            tracked_obstacles.append(
+                DetectedObstacle(bbox_2d, 0, track.label, track.track_id))
+        return True, tracked_obstacles
