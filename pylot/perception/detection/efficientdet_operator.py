0ac77ba752310e9228179e59d463830c4ec19004
Ionel Gog <gogionel@gmail.com>
2020-06-01 13:30:35 -0700

    Updated EfficientDet operator to use the new API.

--- pylot/perception/detection/efficientdet_operator.py
+++ pylot/perception/detection/efficientdet_operator.py
@@ -1,20 +1,9 @@
-""" If this operator needs to be run with the faster NMS implementation,
-follow these steps:
-    1. Clone google/automl and tensorflow/models from Github.
-    2. Run the following commands in automl/efficientdet:
-        a. grep -lIR "from object_detection" ./* | xargs sed -i "s/from object_detection/from object_det/g"
-        b. mv object_detection object_det
-        c. grep -lIR "inference\." ./* | xargs sed -i 's/inference\./infer\./g'
-        d. grep -lIR "import inference" ./* | xargs sed -i 's/import inference/import infer/g'
-        e. mv inference.py infer.py
-        f. grep -lIR " utils" ./* | xargs sed -i 's/ utils/ util/g'
-	g. grep -lIR "utils.TpuBatchNormalization" ./* | xargs sed -i 's/utils\.TpuBatchNormalization/util\.TpuBatchNormalization/g'
-	h. mv utils.py util.py
-    3. Set the MODIFIED_AUTOML flag to on.
-"""
-import erdos
 import time
-import copy
+from collections import deque
+
+import erdos
+
+import tensorflow as tf
 
 import pylot.utils
 from pylot.perception.detection.utils import BoundingBox2D, DetectedObstacle,\
@@ -22,17 +11,11 @@ from pylot.perception.detection.utils import BoundingBox2D, DetectedObstacle,\
 from pylot.perception.messages import ObstaclesMessage
 
 # Detection related imports.
-import numpy as np
-import tensorflow as tf
 import hparams_config
 try:
     import infer
 except ImportError:
     import inference as infer
-import anchors
-
-NUM_CLASSES = 90
-MODIFIED_AUTOML = False
 
 
 class EfficientDetOperator(erdos.Operator):
@@ -52,9 +35,10 @@ class EfficientDetOperator(erdos.Operator):
     """
     def __init__(self, camera_stream, time_to_decision_stream,
                  obstacles_stream, model_names, model_paths, flags):
-        camera_stream.add_callback(self.on_msg_camera_stream,
-                                   [obstacles_stream])
+        camera_stream.add_callback(self.on_msg_camera_stream)
         time_to_decision_stream.add_callback(self.on_time_to_decision_update)
+        erdos.add_watermark_callback([camera_stream], [obstacles_stream],
+                                     self.on_watermark)
         self._flags = flags
         self._logger = erdos.utils.setup_logging(self.config.name,
                                                  self.config.log_file_name)
@@ -70,69 +54,36 @@ class EfficientDetOperator(erdos.Operator):
             'speed limit 30', 'speed limit 60', 'speed limit 90'
         }
 
-        # Build inputs and preprocessing.
-        tf.compat.v1.disable_eager_execution()
-
         assert len(model_names) == len(
             model_paths), 'Model names and paths do not have same length'
         self._models = {}
+        self._driver = None
         for index, model_path in enumerate(model_paths):
             model_name = model_names[index]
-            self._models[model_name] = self.load_model(model_name, model_path,
-                                                       flags)
+            self._models[model_name] = self.load_serving_model(
+                model_name, model_path)
             if index == 0:
                 # Use the first model by default.
-                (self._model_name, self._tf_session, self._image_placeholder,
-                 self._detections_batch) = self._models[model_name]
+                self._model_name, self._driver = self._models[model_name]
         self._unique_id = 0
 
-    def load_model(self, model_name, model_path, flags):
+        self._frame_msgs = deque()
+        self._ttd_msgs = deque()
+
+    def load_serving_model(self, model_name, model_path):
         graph = tf.Graph()
-        # Initialize the Config and Session.
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=flags.
-                                    obstacle_detection_gpu_memory_fraction,
-                                    allow_growth=True)
-
-        tf_session = tf.Session(graph=graph,
-                                config=tf.ConfigProto(gpu_options=gpu_options))
-
-        with graph.as_default():
-            # Get the required arguments to initialize Inference.
-            image_size = hparams_config.get_detection_config(
-                model_name).image_size
-            driver = infer.InferenceDriver(model_name, model_path, image_size,
-                                           NUM_CLASSES)
-
-            image_placeholder = tf.placeholder("uint8", [None, None, 3],
-                                               name="image_tensor")
-            raw_images, images, scales = \
-                EfficientDetOperator.build_inputs_with_placeholder(
-                    image_placeholder, image_size)
-
-            # Build model.
-            class_outputs, box_outputs = infer.build_model(model_name, images)
-            infer.restore_ckpt(tf_session,
-                               model_path,
-                               enable_ema=True,
-                               export_ckpt=None)
-
-            # Build postprocessing.
-            params = copy.deepcopy(driver.params)
-            params.update(dict(batch_size=1))
-            detections_batch = None
-            if MODIFIED_AUTOML:
-                detections_batch = EfficientDetOperator.det_post_process(
-                    params, class_outputs, box_outputs, scales)
-            else:
-                detections_batch = infer.det_post_process(
-                    params,
-                    class_outputs,
-                    box_outputs,
-                    scales,
-                    min_score_thresh=self._flags.
-                    obstacle_detection_min_score_threshold)
-
-        return model_name, tf_session, image_placeholder, detections_batch
+        driver = infer.ServingDriver(
+            graph,
+            model_name,
+            model_path,
+            batch_size=1,
+            use_xla=True,
+            model_params=hparams_config.get_detection_config(
+                model_name).as_dict(),
+            gpu_memory_fraction=self._flags.
+            obstacle_detection_gpu_memory_fraction)
+        driver.load(model_path)
+        return model_name, driver
 
     @staticmethod
     def connect(camera_stream, time_to_decision_stream):
@@ -143,25 +94,26 @@ class EfficientDetOperator(erdos.Operator):
                 camera frames are received.
 
         Returns:
-            :py:class:`erdos.WriteStream`: Stream on which the operator sends
-            :py:class:`~pylot.perception.messages.ObstaclesMessage` messages.
+            [:py:class:`erdos.WriteStream`, :py:class:`erdos.WriteStream`]:
+            Streams on which the operator sends
+            :py:class:`~pylot.perception.messages.ObstaclesMessage` messages
+            for detection and :py:class:`erdos.Message` for runtimes.
         """
         obstacles_stream = erdos.WriteStream()
         return [obstacles_stream]
 
-    def pick_model(self, ttd):
+    def _pick_model(self, ttd):
         """Decides which model to use based on time to decision."""
-        runtimes = [('efficientdet-d7', 262), ('efficientdet-d6', 190),
-                    ('efficientdet-d5', 141), ('efficientdet-d4', 74),
-                    ('efficientdet-d3', 42), ('efficientdet-d2', 24),
-                    ('efficientdet-d1', 20), ('efficientdet-d0', 16)]
+        runtimes = [('efficientdet-d6', 190), ('efficientdet-d5', 141),
+                    ('efficientdet-d4', 74), ('efficientdet-d3', 42),
+                    ('efficientdet-d2', 24)]
         fastest_loaded_model_name = None
         for index, (model_name, runtime) in enumerate(runtimes):
             # Pick the model if it is preloaded and if we have enough time to
             # run it.
             if ttd >= runtime and model_name in self._models:
-                self._logger.debug('Using detection model {}; ttd {}'.format(
-                    model_name, ttd))
+                self._logger.debug(
+                    'Using detection model {}'.format(model_name))
                 return self._models[model_name]
             if model_name in self._models:
                 fastest_loaded_model_name = model_name
@@ -171,21 +123,27 @@ class EfficientDetOperator(erdos.Operator):
             format(fastest_loaded_model_name))
         return self._models[fastest_loaded_model_name]
 
+    def update_model_choice(self, detection_deadline):
+        if self._flags.deadline_enforcement == 'dynamic':
+            self._model_name, self._driver = self._pick_model(
+                detection_deadline)
+        elif self._flags.deadline_enforcement == 'static':
+            self._model_name, self._driver = self._pick_model(
+                self._flags.detection_deadline)
+
     def on_time_to_decision_update(self, msg):
         self._logger.debug('@{}: {} received ttd update {}'.format(
             msg.timestamp, self.config.name, msg))
-        if self._flags.deadline_enforcement == 'dynamic':
-            (self._model_name, self._tf_session, self._image_placeholder,
-             self._detections_batch) = self.pick_model(msg.data)
-        elif self._flags.deadline_enforcement == 'static':
-            (self._model_name, self._tf_session, self._image_placeholder,
-             self._detections_batch) = self.pick_model(
-                 self._flags.detection_deadline)
-        else:
-            return
+        self._ttd_msgs.append(msg)
 
     @erdos.profile_method()
-    def on_msg_camera_stream(self, msg, obstacles_stream):
+    def on_msg_camera_stream(self, msg):
+        self._logger.debug('@{}: {} received message'.format(
+            msg.timestamp, self.config.name))
+        self._frame_msgs.append(msg)
+
+    @erdos.profile_method()
+    def on_watermark(self, timestamp, obstacles_stream):
         """Invoked whenever a frame message is received on the stream.
 
         Args:
@@ -196,45 +154,31 @@ class EfficientDetOperator(erdos.Operator):
                 :py:class:`~pylot.perception.messages.ObstaclesMessage`
                 messages.
         """
-        operator_time_total_start = time.time()
         start_time = time.time()
-        self._logger.debug('@{}: {} received message'.format(
-            msg.timestamp, self.config.name))
-        inputs = msg.frame.as_rgb_numpy_array()
-
-        results = []
-        start_time = time.time()
-        if MODIFIED_AUTOML:
-            (boxes_np, scores_np, classes_np,
-             num_detections_np) = self._tf_session.run(
-                 self._detections_batch,
-                 feed_dict={self._image_placeholder: inputs})
-            num_detections = num_detections_np[0]
-            boxes = boxes_np[0][:num_detections]
-            scores = scores_np[0][:num_detections]
-            classes = classes_np[0][:num_detections]
-            results = zip(boxes, scores, classes)
-        else:
-            outputs_np = self._tf_session.run(
-                self._detections_batch,
-                feed_dict={self._image_placeholder: inputs})[0]
-            for _, x, y, width, height, score, _class in outputs_np:
-                results.append(((y, x, y + height, x + width), score, _class))
+        #ttd_msg = self._ttd_msgs.popleft()
+        frame_msg = self._frame_msgs.popleft()
+        #ttd, detection_deadline = ttd_msg.data
+        #self.update_model_choice(detection_deadline)
+        frame = frame_msg.frame
+        inputs = frame.as_rgb_numpy_array()
+        detector_start_time = time.time()
+        outputs_np = self._driver.serve_images([inputs])[0]
+        detector_end_time = time.time()
+        self._logger.debug("@{}: detector runtime {}".format(
+            timestamp, (detector_end_time - detector_start_time) * 1000))
         obstacles = []
-        for (ymin, xmin, ymax, xmax), score, _class in results:
-            if np.isclose(ymin, ymax) or np.isclose(xmin, xmax):
-                continue
-            if MODIFIED_AUTOML:
-                # The alternate NMS implementation screws up the class labels.
-                _class = int(_class) + 1
+        camera_setup = frame.camera_setup
+        for _, y, x, height, width, score, _class in outputs_np:
+            xmin = int(x)
+            ymin = int(y)
+            xmax = int(x + width)
+            ymax = int(y + height)
             if _class in self._coco_labels:
                 if (score >= self._flags.obstacle_detection_min_score_threshold
                         and
                         self._coco_labels[_class] in self._important_labels):
-                    camera_setup = msg.frame.camera_setup
-                    width, height = camera_setup.width, camera_setup.height
-                    xmin, xmax = max(0, int(xmin)), min(int(xmax), width)
-                    ymin, ymax = max(0, int(ymin)), min(int(ymax), height)
+                    xmin, xmax = max(0, xmin), min(xmax, camera_setup.width)
+                    ymin, ymax = max(0, ymin), min(ymax, camera_setup.height)
                     if xmin < xmax and ymin < ymax:
                         obstacles.append(
                             DetectedObstacle(BoundingBox2D(
@@ -245,7 +189,8 @@ class EfficientDetOperator(erdos.Operator):
                         self._unique_id += 1
                         self._csv_logger.info(
                             "{},{},detection,{},{:4f}".format(
-                                pylot.utils.time_epoch_ms(), msg.timestamp,
+                                pylot.utils.time_epoch_ms(),
+                                timestamp.coordinates[0],
                                 self._coco_labels[_class], score))
             else:
                 self._logger.debug(
@@ -253,89 +198,17 @@ class EfficientDetOperator(erdos.Operator):
 
         if (self._flags.visualize_detected_obstacles
                 or self._flags.log_detector_output):
-            msg.frame.annotate_with_bounding_boxes(msg.timestamp, obstacles,
-                                                   None, self._bbox_colors)
+            frame.annotate_with_bounding_boxes(timestamp, obstacles, None,
+                                               self._bbox_colors)
             if self._flags.visualize_detected_obstacles:
-                msg.frame.visualize(self.config.name,
-                                    pygame_display=pylot.utils.PYGAME_DISPLAY)
+                frame.visualize(self.config.name,
+                                pygame_display=pylot.utils.PYGAME_DISPLAY)
             if self._flags.log_detector_output:
-                msg.frame.save(msg.timestamp.coordinates[0],
-                               self._flags.data_path,
-                               'detector-{}'.format(self.config.name))
+                frame.save(timestamp.coordinates[0], self._flags.data_path,
+                           'detector-{}'.format(self.config.name))
         end_time = time.time()
-        obstacles_stream.send(ObstaclesMessage(msg.timestamp, obstacles, 0))
-        obstacles_stream.send(erdos.WatermarkMessage(msg.timestamp))
+        obstacles_stream.send(ObstaclesMessage(timestamp, obstacles, 0))
+        obstacles_stream.send(erdos.WatermarkMessage(timestamp))
         operator_time_total_end = time.time()
-        self._logger.debug("@{}: runtime of the detector: {}".format(
-            msg.timestamp, (end_time - start_time) * 1000))
         self._logger.debug("@{}: total time spent: {}".format(
-            msg.timestamp,
-            (operator_time_total_end - operator_time_total_start) * 1000))
-
-    @staticmethod
-    def build_inputs_with_placeholder(image, image_size):
-        """ Builds the input image using a placeholder.
-
-        Args:
-            image: A placeholder for the image.
-            image_size: a single integer for image width and height.
-
-        Returns
-            (raw_images, processed_image_placeholder, scales)
-        """
-        raw_images, images, scales = [], [], []
-        raw_images.append(image)
-        image, scale = infer.image_preprocess(image, image_size)
-        images.append(image)
-        scales.append(scale)
-        return raw_images, tf.stack(images), tf.stack(scales)
-
-    @staticmethod
-    def det_post_process(params, class_outputs, box_outputs, scales):
-        from object_detection.core.post_processing import \
-            batch_multiclass_non_max_suppression
-        cls_outputs_all, box_outputs_all = [], []
-        for level in range(params['min_level'], params['max_level'] + 1):
-            cls_outputs_all.append(
-                tf.reshape(class_outputs[level],
-                           [params['batch_size'], -1, params['num_classes']]))
-            box_outputs_all.append(
-                tf.reshape(box_outputs[level], [params['batch_size'], -1, 4]))
-        cls_outputs_all = tf.concat(cls_outputs_all, 1)
-        box_outputs_all = tf.concat(box_outputs_all, 1)
-        probs = tf.math.sigmoid(cls_outputs_all)
-
-        # Generate location of anchors.
-        eval_anchors = tf.transpose(
-            anchors.Anchors(params['min_level'], params['max_level'],
-                            params['num_scales'], params['aspect_ratios'],
-                            params['anchor_scale'],
-                            params['image_size']).boxes)
-        ycenter_a = (eval_anchors[0] + eval_anchors[2]) / 2
-        xcenter_a = (eval_anchors[1] + eval_anchors[3]) / 2
-        ha = eval_anchors[2] - eval_anchors[0]
-        wa = eval_anchors[3] - eval_anchors[1]
-
-        # Generate absolute bboxes in the units of pixels of the image.
-        box_outputs_per_sample = tf.transpose(box_outputs_all[0])
-        ty, tx, th, tw = (box_outputs_per_sample[0], box_outputs_per_sample[1],
-                          box_outputs_per_sample[2], box_outputs_per_sample[3])
-        w, h = tf.math.exp(tw) * wa, tf.math.exp(th) * ha
-        ycenter, xcenter = ty * ha + ycenter_a, tx * wa + xcenter_a
-        ymin, ymax = ycenter - h / 2.0, ycenter + h / 2.0
-        xmin, xmax = xcenter - w / 2.0, xcenter + w / 2.0
-        boxes = tf.transpose(tf.stack([ymin, xmin, ymax, xmax]))
-
-        # Generate the outputs
-        boxes_all = tf.reshape(boxes, [params['batch_size'], -1, 1, 4])
-        probs_all = tf.reshape(
-            probs, [params['batch_size'], -1, params['num_classes']])
-        (boxes_tf, scores_tf, classes_tf, _, _, num_detections_tf) = \
-            batch_multiclass_non_max_suppression(
-                boxes=boxes_all, scores=probs_all, score_thresh=0.5,
-                iou_thresh=0.5,
-                max_size_per_class=anchors.MAX_DETECTIONS_PER_IMAGE,
-                max_total_size=anchors.MAX_DETECTIONS_PER_IMAGE,
-                use_combined_nms=False, use_class_agnostic_nms=True)
-        boxes_tf *= scales
-        return [boxes_tf, scores_tf, classes_tf, num_detections_tf]
+            timestamp, (operator_time_total_end - start_time) * 1000))
