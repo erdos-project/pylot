0d73c3881fb67fcac10a4413a3281eb0cf71e3dd
Sukrit Kalra <sukritkalra@gmail.com>
2020-07-03 16:28:02 -0700

    Including GNSS for sensor fusion in localization

--- pylot/localization/localization_operator.py
+++ pylot/localization/localization_operator.py
@@ -1,5 +1,6 @@
 import erdos
-from pylot.utils import Quaternion, Pose, Transform, Location, Vector3D
+from pylot.utils import Quaternion, Pose, Transform, Location, \
+        Vector3D, Rotation
 
 import numpy as np
 
@@ -58,6 +59,21 @@ class LocalizationOperator(erdos.Operator):
         # values have stabilized.
         self._is_started = False
 
+        ## Constants required for the Kalman filtering.
+        var_imu_f, var_imu_w, var_gnss = 0.10, 0.25, 0.10
+        self.__Q = np.identity(6)
+        self.__Q[0:3, 0:3] = self.__Q[0:3, 0:3] * var_imu_f
+        self.__Q[3:6, 3:6] = self.__Q[3:6, 3:6] * var_imu_w
+
+        self.__F = np.identity(9)
+
+        self.__L = np.zeros([9, 6])
+        self.__L[3:9, :] = np.identity(6)
+
+        self.__R_GNSS = np.identity(3) * var_gnss
+
+        self._last_covariance = np.zeros((9, 9))
+
     @staticmethod
     def connect(imu_stream, gnss_stream, ground_pose_stream):
         pose_stream = erdos.WriteStream()
@@ -81,6 +97,49 @@ class LocalizationOperator(erdos.Operator):
                     "The message for {} with timestamp {} was not found".
                     format(name, timestamp))
         return msg
+
+    def __skew_symmetric(self, v):
+        """Skew symmetric form of a 3x1 vector."""
+        return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]],
+                        dtype=np.float64)
+
+    def __update_using_gnss(self, location_estimate, velocity_estimate,
+                            rotation_estimate, gnss_reading, delta_t):
+        # Construct H_k = [I, 0, 0] (shape=(3, 9))
+        H_k = np.zeros((3, 9))
+        H_k[:, :3] = np.identity(3)
+
+        # Propogate uncertainty.
+        Q = self.__Q * delta_t * delta_t
+        self._last_covariance = (self.__F.dot(self._last_covariance).dot(
+            self.__F.T)) + (self.__L.dot(Q).dot(self.__L.T))
+
+        # Compute Kalman gain. (shape=(9, 3))
+        K_k = self._last_covariance.dot(
+            H_k.T.dot(
+                np.linalg.inv(
+                    H_k.dot(self._last_covariance.dot(H_k.T)) +
+                    self.__R_GNSS)))
+
+        # Compute error state. (9x3) x ((3x1) - (3x1)) = shape(9, 1)
+        delta_x_k = K_k.dot(gnss_reading - location_estimate)
+
+        # Correct predicted state.
+        corrected_location_estimate = location_estimate + delta_x_k[0:3]
+        corrected_velocity_estimate = velocity_estimate + delta_x_k[3:6]
+        roll, pitch, yaw = delta_x_k[6:]
+        corrected_rotation_estimate = Quaternion.from_rotation(
+            Rotation(roll=roll, pitch=pitch, yaw=yaw)) * rotation_estimate
+
+        # Fix the covariance.
+        self._last_covariance = (np.identity(9) - K_k.dot(H_k)).dot(
+            self._last_covariance)
+
+        return (
+            corrected_location_estimate,
+            corrected_velocity_estimate,
+            corrected_rotation_estimate,
+        )
 
     @erdos.profile_method()
     def on_watermark(self, timestamp):
@@ -115,9 +174,9 @@ class LocalizationOperator(erdos.Operator):
             # Estimate the rotation.
             last_rotation_estimate = Quaternion.from_rotation(
                 self._last_pose_estimate.transform.rotation)
-            rotation_estimate = (last_rotation_estimate *
-                                 Quaternion.from_angular_velocity(
-                                     imu_msg.gyro, delta_t)).as_rotation()
+            rotation_estimate = (
+                last_rotation_estimate *
+                Quaternion.from_angular_velocity(imu_msg.gyro, delta_t))
 
             # Transform the IMU accelerometer data from the body frame to the
             # world frame, and retrieve location and velocity estimates.
@@ -129,22 +188,41 @@ class LocalizationOperator(erdos.Operator):
                     as_numpy_array()
 
             # Estimate the location.
-            location_estimate_np = last_location_estimate + (
+            location_estimate = last_location_estimate + (
                 delta_t * last_velocity_estimate) + ((
                     (delta_t**2) / 2.0) * accelerometer_data)
-            location_estimate = Location(*location_estimate_np)
 
             # Estimate the velocity.
-            velocity_estimate_np = last_velocity_estimate + (
-                delta_t * accelerometer_data)
-            velocity_estimate = Vector3D(*velocity_estimate_np)
+            velocity_estimate = last_velocity_estimate + (delta_t *
+                                                          accelerometer_data)
+
+            ## Fuse the GNSS values using an EKF to fix drifts and noise in
+            ## the estimates.
+
+            # Linearize the motion model and compute Jacobians.
+            self.__F[0:3, 3:6] = np.identity(3) * delta_t
+            self.__F[3:6, 6:9] = last_rotation_estimate.matrix.dot(
+                -self.__skew_symmetric(accelerometer_data.reshape(
+                    (3, 1)))) * delta_t
+
+            # Fix estimates using GNSS
+            gnss_reading = Location.from_gps(
+                gnss_msg.latitude, gnss_msg.longitude,
+                gnss_msg.altitude).as_numpy_array()
+            (
+                location_estimate,
+                velocity_estimate,
+                rotation_estimate,
+            ) = self.__update_using_gnss(location_estimate, velocity_estimate,
+                                         rotation_estimate, gnss_reading,
+                                         delta_t)
 
             # Create the PoseMessage and send it downstream.
             current_pose = Pose(
-                transform=Transform(location=location_estimate,
-                                    rotation=rotation_estimate),
-                forward_speed=velocity_estimate.magnitude(),
-                velocity_vector=velocity_estimate,
+                transform=Transform(location=Location(*location_estimate),
+                                    rotation=rotation_estimate.as_rotation()),
+                forward_speed=Vector3D(*velocity_estimate).magnitude(),
+                velocity_vector=Vector3D(*velocity_estimate),
                 localization_time=current_ts,
             )
             self._logger.debug("@{}: Predicted pose: {}".format(
