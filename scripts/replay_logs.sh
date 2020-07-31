14d051d62c67cf3b5330c08c317b7ee7a9165bce
Ionel Gog <gogionel@gmail.com>
2020-07-30 22:35:27 -0700

    Updated the replay script to replay individual files.

--- scripts/replay_logs.sh
+++ scripts/replay_logs.sh
@@ -2,9 +2,19 @@
 # $1 Directory where the log files are
 
 cd ${CARLA_HOME}/PythonAPI/examples
-for FILE_NAME in `ls -d $1/*.log`; do
+
+function replay_log {
+    FILE_NAME=$1
     echo "Replaying ${FILE_NAME}"
     HERO_ID=`python3 show_recorder_file_info.py -f ${FILE_NAME} | grep -B 5 hero | head -n 1 | cut -d' ' -f3 | cut -d':' -f1`
     python3 start_replaying.py -f ${FILE_NAME} -c ${HERO_ID} -x 5
     read -p "Press any key when the replay completes... " -n1 -s
-done
+}
+
+if [ -f $1 ]; then
+    replay_log $1
+else
+    for FILE_NAME in `ls -d $1/*.log`; do
+        replay_log $FILE_NAME
+    done
+fi
