6001b49d30bfb880ee34c6b428c5dec80f3d7678
Ionel Gog <gogionel@gmail.com>
2020-07-31 00:11:46 -0700

    [Bug] Clip values before passing them to asin.

--- pylot/utils.py
+++ pylot/utils.py
@@ -572,7 +572,7 @@ class Transform(object):
             self.forward_vector = Vector3D(self.matrix[0, 0],
                                            self.matrix[1, 0], self.matrix[2,
                                                                           0])
-            pitch_r = math.asin(self.forward_vector.z)
+            pitch_r = math.asin(np.clip(self.forward_vector.z, -1, 1))
             yaw_r = math.acos(
                 np.clip(self.forward_vector.x / math.cos(pitch_r), -1, 1))
             roll_r = math.asin(
