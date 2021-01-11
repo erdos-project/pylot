3fa6ff5a18f02db5f1cd5919d9d8861a5a970060
Shaviv-Hoffman-Lowitz <33563242+Shaviv-Hoffman-Lowitz@users.noreply.github.com>
2021-01-11 15:24:49 -0800

    Add a port argument to the run_simulator script (#153)

--- scripts/run_simulator.sh
+++ scripts/run_simulator.sh
@@ -5,4 +5,10 @@ if [ -z "$CARLA_HOME" ]; then
     exit 1
 fi
 
-SDL_VIDEODRIVER=offscreen ${CARLA_HOME}/CarlaUE4.sh -opengl -windowed -ResX=800 -ResY=600 -carla-server -benchmark -fps=20 -quality-level=Epic
+if [ -z "$1" ]; then
+    PORT=2000
+else
+    PORT=$1
+fi
+
+SDL_VIDEODRIVER=offscreen ${CARLA_HOME}/CarlaUE4.sh -opengl -windowed -ResX=800 -ResY=600 -carla-server -world-port=$PORT -benchmark -fps=20 -quality-level=Epic
