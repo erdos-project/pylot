eb503be387b223ede2d64aebee9a085ffaaeca38
mageofboy <leong.eric17@berkeley.edu>
2021-04-23 19:33:24 +0000

    model working, need fix operator

--- pylot/perception/flags.py
+++ pylot/perception/flags.py
@@ -87,7 +87,11 @@ flags.DEFINE_enum('center_track_model', 'kitti_tracking',
 # QDTrack tracking flags.
 flags.DEFINE_string(
     'qd_track_model_path',
-    'dependencies/models/tracking/CenterTrack/kitti_fulltrain.pth', #FIX
+    'dependencies/models/tracking/qd_track/qdtrack-frcnn_r50_fpn_12e_bdd100k-13328aed.pth', #FIX
+    'Path to the model')
+flags.DEFINE_string(
+    'qd_track_config_path',
+    'dependencies/qdtrack/configs/qdtrack-frcnn_r50_fpn_12e_bdd100k.py', #FIX
     'Path to the model')
 
 # Lane detection flags.
