ef49d55f5d37c9d11d3c3b3c2583aa06c783212f
Josh <josh.li.pwns@gmail.com>
2021-06-14 20:26:06 -0400

    added ROSCameraPublisher.py and visualizer operator can publish camera frame data to foxglove-studio

---
+++ pylot/debug/ROSCameraPublisher.py
@@ -0,0 +1,23 @@
+import sys
+import rospy
+import numpy as np
+from sensor_msgs.msg import Image
+
+class ROSCameraPublisher:
+
+    def __init__(self, topic:str):
+        # publishes to the given topic 
+        self.image_pub = rospy.Publisher(topic, Image, queue_size=10)
+        
+    def publish(self, img_arr):
+        # converts the 3d np arrary img_arr to a sensor_msgs/Image datatype
+        img_msg = Image(encoding='rgb8')
+        img_msg.height, img_msg.weight, channels = img_arr.shape
+        contig_arr = np.ascontiguousarray(img_arr)
+        img_msg.data = contig_arr.tobytes()
+        img_msg.step = img_msg.width * img_msg.height 
+        img_msg.is_bigendian = (
+            img_arr.dtype.byteorder == '>' or 
+            img_arr.dtype.byteorder == '=' and sys.byteorder == 'big'
+        )
+        self.image_pub.publish(img_msg)
