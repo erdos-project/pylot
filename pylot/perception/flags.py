aaf900210c6911c3bbbee9e655b39933f09d2e1b
mageofboy <leong.eric17@berkeley.edu>
2021-07-17 04:41:14 +0000

    lint

--- pylot/perception/flags.py
+++ pylot/perception/flags.py
@@ -86,10 +86,8 @@ flags.DEFINE_enum('center_track_model', 'kitti_tracking',
 
 # QDTrack tracking flags.
 flags.DEFINE_string(
-    'qd_track_model_path',
-    'dependencies/models/tracking/qd_track/' +
-    'qdtrack-frcnn_r50_fpn_12e_bdd100k-13328aed.pth',
-    'Path to the model')
+    'qd_track_model_path', 'dependencies/models/tracking/qd_track/' +
+    'qdtrack-frcnn_r50_fpn_12e_bdd100k-13328aed.pth', 'Path to the model')
 flags.DEFINE_string(
     'qd_track_config_path',
     'dependencies/qdtrack/configs/qdtrack-frcnn_r50_fpn_12e_bdd100k.py',
