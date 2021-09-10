43c714470f77676d25eb0b23c20ded424ae212f0
objorkman <oscarb@berkeley.edu>
2021-09-10 19:21:42 +0000

    Using converted model

--- pylot/perception/detection/detection_operator.py
+++ pylot/perception/detection/detection_operator.py
@@ -40,42 +40,30 @@ class DetectionOperator(erdos.Operator):
         self._logger = erdos.utils.setup_logging(self.config.name,
                                                  self.config.log_file_name)
         self._obstacles_stream = obstacles_stream
-        self._detection_graph = tf.Graph()
-        # Load the model from the model file.
-        pylot.utils.set_tf_loglevel(logging.ERROR)
-        with self._detection_graph.as_default():
-            od_graph_def = tf.compat.v1.GraphDef()
-            with tf.io.gfile.GFile(model_path, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
 
-        self._gpu_options = tf.compat.v1.GPUOptions(
-            allow_growth=True,
-            visible_device_list=str(self._flags.obstacle_detection_gpu_index),
-            per_process_gpu_memory_fraction=flags.
-            obstacle_detection_gpu_memory_fraction)
-        # Create a TensorFlow session.
-        self._tf_session = tf.compat.v1.Session(
-            graph=self._detection_graph,
-            config=tf.compat.v1.ConfigProto(gpu_options=self._gpu_options))
-        # Get the tensors we're interested in.
-        self._image_tensor = self._detection_graph.get_tensor_by_name(
-            'image_tensor:0')
-        self._detection_boxes = self._detection_graph.get_tensor_by_name(
-            'detection_boxes:0')
-        self._detection_scores = self._detection_graph.get_tensor_by_name(
-            'detection_scores:0')
-        self._detection_classes = self._detection_graph.get_tensor_by_name(
-            'detection_classes:0')
-        self._num_detections = self._detection_graph.get_tensor_by_name(
-            'num_detections:0')
+        physical_devices = tf.config.experimental.list_physical_devices('GPU') 
+        # Equivalent to allow_growth=True
+        for device in physical_devices:
+            tf.config.experimental.set_memory_growth(device, True)
+
+        # Load the model from the model file.
+        # pylot.utils.set_tf_loglevel(logging.ERROR)
+        # with self._detection_graph.as_default():
+        #     od_graph_def = tf.compat.v1.GraphDef()
+        #     with tf.io.gfile.GFile(model_path, 'rb') as fid:
+        #         serialized_graph = fid.read()
+        #         od_graph_def.ParseFromString(serialized_graph)
+        #         tf.import_graph_def(od_graph_def, name='')
+
+        self._model = tf.saved_model.load('pylot/perception/detection/converted_model/')
+
         self._coco_labels = load_coco_labels(self._flags.path_coco_labels)
         self._bbox_colors = load_coco_bbox_colors(self._coco_labels)
         # Unique bounding box id. Incremented for each bounding box.
         self._unique_id = 0
-        # Serve some junk image to load up the model.
-        self.__run_model(np.zeros((108, 192, 3)))
+
+        # # Serve some junk image to load up the model.
+        # self.__run_model(np.zeros((108, 192, 3)))
 
     @staticmethod
     def connect(camera_stream: erdos.ReadStream,
@@ -172,12 +160,14 @@ class DetectionOperator(erdos.Operator):
         # Expand dimensions since the model expects images to have
         # shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image_np, axis=0)
-        (boxes, scores, classes, num_detections) = self._tf_session.run(
-            [
-                self._detection_boxes, self._detection_scores,
-                self._detection_classes, self._num_detections
-            ],
-            feed_dict={self._image_tensor: image_np_expanded})
+
+        infer = self._model.signatures['serving_default']
+        result = infer(tf.convert_to_tensor(value=image_np_expanded))
+
+        boxes = result['boxes']
+        scores = result['scores']
+        classes = result['classes']
+        num_detections = result['detections']
 
         num_detections = int(num_detections[0])
         res_classes = [int(cls) for cls in classes[0][:num_detections]]
