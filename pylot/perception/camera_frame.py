eeda1999da3df888948eaef7f1cc39c18eeeb9b8
Eric Leong <leong.eric17@berkeley.edu>
2022-03-30 19:29:14 +0000

    draw line

--- pylot/perception/camera_frame.py
+++ pylot/perception/camera_frame.py
@@ -1,5 +1,5 @@
 import os
-from typing import Tuple, Union
+from typing import List, Tuple, Union
 
 import PIL.Image as Image
 
@@ -124,6 +124,13 @@ class CameraFrame(object):
                     color,
                     thickness=1,
                     lineType=cv2.LINE_AA)
+
+    def draw_line(self, points: List[Vector2D], color, thickness: float = 3):
+        # no negatives
+        assert len(points) >= 2, "Need atleast two points to draw a line"
+        draw_points = np.array([[point.x, point.y] for point in points])
+        cv2.polylines(self.frame, np.array([draw_points], dtype=np.int32),
+                      False, color, thickness=thickness)
 
     def in_frame(self, point: Vector2D) -> bool:
         """Checks if a point is within the frame."""
