1cfa331102e88a844e1ea584fad10881b5a336d4
Oscar Bjorkman <oscarb@berkeley.edu>
2022-04-13 22:43:46 +0000

    Update operator

--- pylot/perception/tracking/qd_track_operator.py
+++ pylot/perception/tracking/qd_track_operator.py
@@ -1,20 +1,21 @@
 import time
 
 import erdos
+from erdos.context import OneInOneOutContext
+from erdos.operator import OneInOneOut
+
+from pylot.perception.camera_frame import CameraFrame
 
 from pylot.perception.detection.obstacle import Obstacle
 from pylot.perception.detection.utils import BoundingBox2D, \
     OBSTACLE_LABELS
-from pylot.perception.messages import ObstaclesMessage
+from pylot.perception.messages import ObstaclesMessageTuple
 
 
-class QdTrackOperator(erdos.Operator):
-    def __init__(self, camera_stream, obstacle_tracking_stream, flags,
-                 camera_setup):
+class QdTrackOperator(OneInOneOut):
+    def __init__(self, flags, camera_setup):
         from qdtrack.apis import init_model
 
-        camera_stream.add_callback(self.on_frame_msg,
-                                   [obstacle_tracking_stream])
         self._flags = flags
         self._logger = erdos.utils.setup_logging(self.config.name,
                                                  self.config.log_file_name)
@@ -29,24 +30,15 @@ class QdTrackOperator(erdos.Operator):
                         'bicycle', 'motorcycle', 'train')
         self.frame_id = 0
 
-    @staticmethod
-    def connect(camera_stream):
-        obstacle_tracking_stream = erdos.WriteStream()
-        return [obstacle_tracking_stream]
-
-    def destroy(self):
-        self._logger.warn('destroying {}'.format(self.config.name))
-
-    @erdos.profile_method()
-    def on_frame_msg(self, msg, obstacle_tracking_stream):
+    def on_data(self, context: OneInOneOutContext, data: CameraFrame):
         """Invoked when a FrameMessage is received on the camera stream."""
         from qdtrack.apis import inference_model
 
         self._logger.debug('@{}: {} received frame'.format(
-            msg.timestamp, self.config.name))
-        assert msg.frame.encoding == 'BGR', 'Expects BGR frames'
+            context.timestamp, self.config.name))
+        assert data.encoding == 'BGR', 'Expects BGR frames'
         start_time = time.time()
-        image_np = msg.frame.as_bgr_numpy_array()
+        image_np = data.as_bgr_numpy_array()
         results = inference_model(self.model, image_np, self.frame_id)
         self.frame_id += 1
 
@@ -70,5 +62,9 @@ class QdTrackOperator(erdos.Operator):
                              track_id,
                              bounding_box_2D=bounding_box_2D))
         runtime = (time.time() - start_time) * 1000
-        obstacle_tracking_stream.send(
-            ObstaclesMessage(msg.timestamp, obstacles, runtime))
+        context.write_stream.send(
+            erdos.Message(context.timestamp,
+                          ObstaclesMessageTuple(obstacles, runtime)))
+
+    def destroy(self):
+        self._logger.warn('destroying {}'.format(self.config.name))
